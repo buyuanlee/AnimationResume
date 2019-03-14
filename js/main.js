c5352004183694ee9be6d81d2c6501164f9091b2
buyuanlee <buyuanlee@gmail.com>
2019-03-14 18:54:08 +0800

    修复bug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 !function () {
-    let speed = 50
+    let speed = 50;
 
     writeCode('', cssCode, () => {
         createPaper(() => {
@@ -14,39 +14,39 @@
                 })
             })
         })
-    })
+    });
 
     function writeCode(prefix, code, fn) {
-        let domCode = document.querySelector('#code')
-        let n = 0
+        let domCode = document.querySelector('#code');
+        let n = 0;
         var clock = setInterval(() => {
-            n += 1
-            domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css)
-            myStyle.innerHTML = prefix + code.substring(0, n)
-            domCode.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"})
+            n += 1;
+            domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
+            myStyle.innerHTML = prefix + code.substring(0, n);
+            domCode.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"});
             if (n >= code.length) {
-                window.clearInterval(clock)
+                window.clearInterval(clock);
                 fn.call()
             }
         }, speed)
     }
 
     function createPaper(fn) {
-        var paper = document.createElement('pre')
-        paper.id = 'paper'
-        document.querySelector('#box').appendChild(paper)
+        var paper = document.createElement('pre');
+        paper.id = 'paper';
+        document.querySelector('#box').appendChild(paper);
         fn.call()
     }
 
     function writeMarkdown(markdown, fn) {
-        let domPaper = document.querySelector('#paper')
-        let n = 0
+        let domPaper = document.querySelector('#paper');
+        let n = 0;
         var clock = setInterval(() => {
-            n += 1
-            domPaper.innerHTML = Prism.highlight(markdown.substring(0, n), Prism.languages.markdown)
-            domPaper.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"})
+            n += 1;
+            domPaper.innerHTML = Prism.highlight(markdown.substring(0, n), Prism.languages.markdown);
+            domPaper.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"});
             if (n >= markdown.length) {
-                window.clearInterval(clock)
+                window.clearInterval(clock);
                 fn.call()
             }
         }, speed)
@@ -54,9 +54,9 @@
     }
 
     function convertMarkdownToHtml(fn) {
-        document.querySelector('#paper').innerHTML = marked(markdown)
+        document.querySelector('#paper').innerHTML = marked(markdown);
         fn.call()
     }
 
 
-}.call()
+}.call();
