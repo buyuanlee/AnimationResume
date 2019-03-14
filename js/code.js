feb9e3293efbe06dfa73c702e71ac86bbc2b5b8d
buyuanlee <buyuanlee@gmail.com>
2019-03-14 19:04:30 +0800

    优化内容

--- js/code.js
+++ js/code.js
@@ -37,7 +37,7 @@ html{
 }
 /*现在开始写简历吧*/
 /*首先我需要一张纸*/
-`
+`;
     var htmlCode = window.htmlCode = `
 #code{
     display: inline-block;
@@ -79,72 +79,49 @@ html{
 #paper{
     margin-top: 0;
 }
-`
+`;
     var markdown = window.markdown = `
 # 个人简历
 
 ### 基本信息
-
 - **姓名**：李建伟
-
 - **性别**：男
-
 - **出生日期**：1995
-
 - **目标职位**：前端工程师
 
-
 ### 联系方式：
-
 - **手机**：17557290656	
 - **邮箱**：buyuanwanli@foxmail.com
 - **QQ**：152927520
 - **微信**: ecjean
 
-
-
 ### 技能描述
-
 - **小程序制作**
   - 可以独立进行小程序开发，了解基本相关API或组件的使用
-
 - **Vue**
   - 熟悉Vue的常用功能，如组件、Vue-Router、双向绑定等
-
 - **jQuery**
   - 熟悉jQuery的常用API，能使用jQuery制作网站、轮播、Tap组件等
-
 - **HTML5**&CSS3
   - 能独立制作精美网页，掌握CSS3动画，过渡效果、响应式等常用技术
-
 - **移动端页面**
   - 会使用REM、vw/vh、FastClick等技术制作适配手机设备的页面
-
 - **前端框架**
   - 理解MVC、MVVM等思想，可以熟练使用相关的库，如Vue、React
 
 ### 教育经历
-
 - 安徽大学江淮学院
   - 电子商务专业
-
-### 工作经历
-
-- 2014-2017
-  - 淘宝运营
-- 2018
-  - 自学前端
 
 ### 项目展示
 - 作品一
 - 作品二
 - 作品散
-
-`
+`;
     var changeCode = window.changeCode = `
 /*这样看起来很不舒服？让我们把markdown转换成更易读的显示方式吧*/
-`
+`;
     var conclusion = window.conclusion = `
 /*好了，这个就是我的简历了。如果您对我还算满意或者想要更多了解的话，请联系我哦*/
 `
-}.call()
+}.call();
